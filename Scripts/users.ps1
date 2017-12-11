e00615d7f9e0e46254561907df241c06998dd5cf
Yvar de Goffau <yvar.degoffau@viacesi.fr>
2017-12-11 10:01:50 +0100

    Added a verification for an existing user

--- Scripts/users.ps1
+++ Scripts/users.ps1
@@ -3,6 +3,10 @@ $users = Import-CSV $filepath -delimiter ","
 
 Foreach ($user in $users) {
      write-host $user.Name $user.Surname
-     New-ADUser -Title $user.Title -Name $user.Name -Surname $user.Surname -EmailAddress $user.EmailAddress -OfficePhone $user.OfficePhone -MobilePhone $user.MobilePhone -HomePhone $user.HomePhone -Office $user.Office -Department $user.Department -Division $user.Division -City $user.City -State $user.State -Country $user.Country -Company $user.Company -Organization $user.Organization 
+     $username=$user.name
+     $exists=get-ADUser -Filter "Name -eq '$username'"
+     if($exists -eq $null){
+        New-ADUser -Name $user.Name -Surname $user.Surname -EmailAddress $user.EmailAddress -OfficePhone $user.OfficePhone -MobilePhone $user.MobilePhone -HomePhone $user.HomePhone
+        }
 }
 
